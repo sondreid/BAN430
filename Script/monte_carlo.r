818ceb1348d6440b1db53e558cb81ac10d45ded6
Sondre Eide <sondre.eide@gmail.com>
2021-03-28 12:12:43 +0200

    Basic sampling

--- Script/monte_carlo.r
+++ Script/monte_carlo.r
@@ -24,7 +24,7 @@ n <- 240
 fit <- arima_fit
 generate_y <- function(fit, n, d = 1, sd = 1) {
     #' 
-    #' Returns vector of residuals + random component
+    #' 
     sigma <- sd(residuals(fit)$.resid)
     resids <- residuals(fit)$.resid
     mean_resid <- mean(residuals(fit)$.resid)
@@ -61,7 +61,7 @@ generate_y <- function(fit, n, d = 1, sd = 1) {
 generate_y(arima_fit, 216, 1)
 
 plot(generate_y(arima_fit, 216), type = "l")
-library(foreach)
+
 
 
 determineTrainTest <- function(h = 24, maxlength = 19) {
@@ -74,36 +74,30 @@ determineTrainTest <- function(h = 24, maxlength = 19) {
 determineTrainTest()
 
 
-simulate <- function(R = 100, train_length = 216, h = 24) {
+simulate <- function(R = 1, h = 24) {
     cl <- parallel::makeCluster(8)                                                                                         ### Make clusters
     doParallel::registerDoParallel(cl)
     res <- matrix(0,2,1)
     colnames(res) <- c("MSE")
     rownames(res)<- c("VAR multivariate", "ARIMA yt")
     for(i in 1:R){
-        num_h <- sample(1:19, 1)*12
-        train <- num_h - 24
-        train_range <- c((1:train))
-        test_range <-  c((train+1):num_h)
-
-
-        y <- generate_y(arima_fit, train_length+h)
-        y_e <- y[1:train_length]
-        y_t <- y[(train_length+1):(train_length+h)]
+        sample <- (sample(6:19, 1))*12                                                                                    ### Change sample size
+        train_length <- sample - h                                                                                         ### Set length of training set
+        train_range <- c((1:train_length))
+        test_range <-  c((train_length+1):sample)
+        y <- generate_y(arima_fit, sample)
+        y_e <- y[train_range]
+        y_t <- y[test_range]
         x <- c()
         x[1] <- 0
         for (j in 2:(train_length+h)) {
             x[j] <- 0.5*y[j-1] + 0.5*x[j-1] 
         }
-        x_e <- x[1:train_length]
-        x_t <- x[(train_length+1):(train_length+h)]
-        data_x_y = data.frame(date = unemployment_train_ts$date, x_e = x_e, y_e = y_e)  %>%  as_tsibble(index = date)
-        ar_term <- VARselect(data_x_y[,2:3], lag.max =10, type="const")[["selection"]] # Confirming AR term
+        x_e <- x[train_range]
+        x_t <- x[test_range]
+        data_x_y = data.frame(num = train_range, x_e = x_e, y_e = y_e)  %>%  as_tsibble(index = num)
+        ar_term <- VARselect(data_x_y[,2:3], lag.max =10, type="const")[["selection"]]                                    ### Find optimal AR term
         var_multi  <- vars:: VAR(data_x_y[,2:3], p  = ar_term[[2]])
-        #var_multi <- data_x_y %>% model(var_multi = fable::VAR(vars(y_e, x_e) ~ AR(p = 0:5)))
-        # print(paste("y_t", y_t))
-        # print(paste("y_e", y_e))
-        # print(y_t- predict(var_multi, n.ahead = 24)$fcst$y_e)
         if (any(is.na(predict(var_multi, n.ahead = 24)$fcst$y_e))) { 
             next
         }
@@ -116,43 +110,12 @@ simulate <- function(R = 100, train_length = 216, h = 24) {
     parallel::stopCluster(cl)
     return(res)
 }
-
+simulate()
 sim_res <- simulate()
 
 sim_res  %>% 
   kbl(caption = "Metrics of Monte Carlo simulated forecasts on generated data", digits = 2) %>%
   kable_classic(full_width = F, html_font = "Times new roman")
-
-
-
-
-
-
-
-
-
-#### UNDER TESTING
-y <- arima.sim(model = list(
-                            ar = c(0.07207483, 0.18150995, 0.07974563, 0.04508594, 0.28128337),
-                            ma = c(-0.87115053)), n = 240,
-                            n.start = 24,
-                            start.innov = augment(arima_fit)$.innov)
-
-
-plot(y)
-
-?arima.sim
-
-report(arima_fit)
-cov(residuals(arima_fit)$.resid)
-
-library(mvtnorm)
-data(finland)
-test <- VECM(finland, lag=2, estim="ML", r=1)
-sigma <- cov(test$residuals)
-rmvnorm(n+b, mean =rep(0,nrow(sigma)), sigma = sigma)
-test$k
-
 
 
 
